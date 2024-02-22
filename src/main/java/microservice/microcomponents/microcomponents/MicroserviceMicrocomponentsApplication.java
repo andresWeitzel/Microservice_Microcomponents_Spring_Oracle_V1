eaac4b74a420004b3e1b0d5c304e03efffcc67bc
Andrés Weitzel <andres96energy@hotmail.com>
2024-02-22 01:42:04 -0300

    Added changes

---
+++ src/main/java/microservice/microcomponents/microcomponents/MicroserviceMicrocomponentsApplication.java
@@ -0,0 +1,13 @@
+package microservice.microcomponents.microcomponents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceMicrocomponentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceMicrocomponentsApplication.class, args);
+	}
+
+}
